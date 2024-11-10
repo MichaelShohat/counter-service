29e6d9628f6f10bb6a2a6a33957ab867f4572806
Michael Shohat <michaelsho9@gmail.com>
2024-11-10 11:28:42 +0200

    Add a counter for get requests

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # counter-service
-Repo containing the code for the counter-service app
+This code contains a simple web server that counts the number of POST and GET requests it serves and returns the counter on every GET request it receives. 
--- counter-service.py
+++ counter-service.py
@@ -2,14 +2,16 @@
 from flask import Flask, request, request_started
 
 app = Flask(__name__)
-counter = 0
+counter_post = 0
+counter_get = 0
 @app.route('/', methods=["POST", "GET"])
 def index():
-    global counter
+    global counter_post, counter_get
     if request.method == "POST":
-        counter+=1
+        counter_post+=1
         return "Hmm, Plus 1 please "
     else:
-        return str(f"Our counter is: {counter} ")
+        counter_get+=1
+        return str(f"Our POST counter is: {counter_post} and our GET counter is: {counter_get}")
 if __name__ == '__main__':
     app.run(debug=True,port=80,host='0.0.0.0')
